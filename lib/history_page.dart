d99a8e2f4b2740e440fd0511444f0a30a0132dc3
Fian <fiannisa@mhs.unsyiah.ac.id>
2024-04-19 18:22:03 +0700

    membuat container untuk menampilkan riwayat pemantauan dengan looping

--- lib/history_page.dart
+++ lib/history_page.dart
@@ -328,6 +328,301 @@ class _HistoryPageState extends State<HistoryPage> {
                   ],
                 ),
               ),
+              Container(
+                width: double.infinity, // menetapkan lebar menjadi penuh
+                child: Transform.translate(
+                  offset: Offset(0.0, -45.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 342,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Color(0x40000000),
+                                      blurRadius: 2,
+                                      offset: Offset(0, 4),
+                                    ),
+                                  ],
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    // Jarak dari atas untuk teks "Riwayat Pemantauan"
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          left: 15.0,
+                                          top: 30.0,
+                                          right: 15.0,
+                                          bottom: 20.0),
+                                      child: Column(
+                                        children: [
+                                          // menampilkan judul container
+                                          Row(
+                                            children: [
+                                              Text(
+                                                'Riwayat Pemantauan Tanggal',
+                                                style: TextStyle(
+                                                  color: Color(0xFF2490A9),
+                                                  fontSize: 12.0,
+                                                  fontFamily: 'Poppins',
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              Text(
+                                                ' $selectedDate',
+                                                style: TextStyle(
+                                                  color: Colors.black,
+                                                  fontSize: 12.0,
+                                                  fontFamily: 'Poppins',
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          SizedBox(height: 27.0),
+
+                                          // Menampilkan setiap data pemantauan
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 10.0),
+                                            child: Column(
+                                              children:
+                                                  listData.asMap().entries.map(
+                                                (entry) {
+                                                  int index = entry.key;
+                                                  List<dynamic> data =
+                                                      entry.value;
+                                                  Color color1 =
+                                                      (data[1] < 6.0 ||
+                                                              data[1] > 8.0)
+                                                          ? Colors.red
+                                                          : Color(0xFF2490A9);
+                                                  String status1 =
+                                                      (data[1] < 6.0)
+                                                          ? "Rendah"
+                                                          : "Netral";
+                                                  Color color2 =
+                                                      (data[2] < 6.0 ||
+                                                              data[2] > 8.0)
+                                                          ? Colors.red
+                                                          : Color(0xFF2490A9);
+                                                  String status2 =
+                                                      (data[2] < 6.0)
+                                                          ? "Rendah"
+                                                          : "Netral";
+                                                  return Row(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment.start,
+                                                    children: [
+                                                      Column(
+                                                        children: [
+                                                          Container(
+                                                            width: 16,
+                                                            height: 16,
+                                                            decoration:
+                                                                BoxDecoration(
+                                                              shape: BoxShape
+                                                                  .circle,
+                                                              color: Color(
+                                                                  0xFF2490A9),
+                                                            ),
+                                                          ),
+                                                          if (index !=
+                                                              listData.length -
+                                                                  1)
+                                                            SizedBox(
+                                                                height: 29.0,
+                                                                width: 2.0,
+                                                                child: Container(
+                                                                    color: Color(
+                                                                        0xFF2490A9))),
+                                                        ],
+                                                      ),
+                                                      SizedBox(width: 5.0),
+                                                      Column(
+                                                        crossAxisAlignment:
+                                                            CrossAxisAlignment
+                                                                .start,
+                                                        children: [
+                                                          Text(
+                                                            '${data[0]} WIB',
+                                                            style: TextStyle(
+                                                              color: Color(
+                                                                  0xFF2490A9),
+                                                              fontSize: 11.0,
+                                                              fontFamily:
+                                                                  'Poppins',
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .w700,
+                                                            ),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                      SizedBox(width: 5.0),
+                                                      Column(
+                                                        crossAxisAlignment:
+                                                            CrossAxisAlignment
+                                                                .start,
+                                                        mainAxisAlignment:
+                                                            MainAxisAlignment
+                                                                .start,
+                                                        children: [
+                                                          Text(
+                                                            'pH Input',
+                                                            style: TextStyle(
+                                                              color: Color(
+                                                                  0xFF2490A9),
+                                                              fontSize: 11.0,
+                                                              fontFamily:
+                                                                  'Poppins',
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .w400,
+                                                            ),
+                                                          ),
+                                                          Text(
+                                                            'pH Output',
+                                                            style: TextStyle(
+                                                              color: Color(
+                                                                  0xFF2490A9),
+                                                              fontSize: 11.0,
+                                                              fontFamily:
+                                                                  'Poppins',
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .w400,
+                                                            ),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                      SizedBox(width: 12.0),
+                                                      Column(
+                                                        crossAxisAlignment:
+                                                            CrossAxisAlignment
+                                                                .start,
+                                                        children: [
+                                                          Text(
+                                                            '${data[1]} ${status1}',
+                                                            style: TextStyle(
+                                                              color: color1,
+                                                              fontSize: 11.0,
+                                                              fontFamily:
+                                                                  'Poppins',
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .w700,
+                                                            ),
+                                                          ),
+                                                          Text(
+                                                            '${data[2]} ${status2}',
+                                                            style: TextStyle(
+                                                              color: color2,
+                                                              fontSize: 11.0,
+                                                              fontFamily:
+                                                                  'Poppins',
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .w700,
+                                                            ),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                    ],
+                                                  );
+                                                },
+                                              ).toList(),
+                                            ),
+                                          ),
+                                          SizedBox(height: 16.0),
+
+                                          // Menampilkan tombol Selengkap-nya
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            children: [
+                                              ElevatedButton(
+                                                onPressed: () {
+                                                  // Implementasi aksi ketika tombol ditekan
+                                                  Navigator.pushNamed(
+                                                      context, '/history');
+                                                },
+                                                style: ElevatedButton.styleFrom(
+                                                  shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            15),
+                                                  ),
+                                                  padding: EdgeInsets
+                                                      .zero, // Mengatur padding menjadi nol
+                                                  minimumSize: Size(0,
+                                                      0), // Mengatur ukuran minimum menjadi nol
+                                                  tapTargetSize:
+                                                      MaterialTapTargetSize
+                                                          .shrinkWrap, // Mengurangi area tap
+                                                  backgroundColor: Color(
+                                                      0xFF2490A9), // Mengatur warna latar belakang
+                                                ).copyWith(
+                                                  backgroundColor:
+                                                      MaterialStateProperty
+                                                          .resolveWith<Color>(
+                                                              (states) {
+                                                    if (states.contains(
+                                                        MaterialState
+                                                            .pressed)) {
+                                                      // Ketika tombol ditekan
+                                                      return Colors
+                                                          .blueGrey; //Warna abu-abu muda
+                                                    }
+                                                    ;
+                                                    return Color(
+                                                        0xFF2490A9); // Warna asli
+                                                  }),
+                                                ),
+                                                child: Padding(
+                                                  padding: EdgeInsets.fromLTRB(
+                                                      18.0,
+                                                      8.0,
+                                                      18.0,
+                                                      8.0), // Tambahkan padding ke teks
+                                                  child: Text(
+                                                    'Selengkap-nya',
+                                                    style: TextStyle(
+                                                      fontSize: 10,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      color: Colors.white,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ), // Spasi antara container daftar dan bagian bawah
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
               ],
           ),
         ),
